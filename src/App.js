cd3410dab7b9350feb2062d935fdf72eab37a55e
nikolapivac <npivac00@fesb.hr>
2023-02-22 19:15:34 +0100

    Added "Add Task" functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+  }
+
+  const addNewTask = () => {
+    setTodoList([...todoList, newTask]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="form">
+        <input onChange={handleChange} />
+        <button onClick={addNewTask}>ADD</button>
+      </div>
+      <div className='todo-list'>
+        {todoList.map((task) => {
+          return <p>{task}</p>
+        })}
+      </div>
     </div>
   );
 }
